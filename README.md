bb9fb43fa3d5aa922668b4d35c06b0e653225c8b
TheBharatMishra <mishrabm2004@gmail.com>
2024-06-24 19:42:17 +0530

    MOre

---
+++ README.md
@@ -0,0 +1,3 @@
+# RUST Learning
+
+These are just the rough work i did while learning rust programming language.
--- send_btc/src/main.rs
+++ send_btc/src/main.rs
@@ -1,15 +1,51 @@
 use std::io;
+use rand::Rng;
+
+fn send_bitcoin(){
+    println!("We are sending some bitcoin");
+
+    let clients=vec!["homer","marge","bart","lisa"];
+
+    println!("Who do you want to send Bitcoin to?]n");
+    for client in &clients{
+        println!("{}",client);
+    }
+
+    println("\n");
+}
+
+fn recieve_bitcoin(){
+println!("\nWe are going to recieve some bitcoin\n");
+
+let amount = rand::thread_rng().gen_range(1,10);
+
+println!("You just recieved {} BTC!\n",amount);
+}
+
+fn exit_console(){
+    println!("Invalid option must be 'r' or 's'.");
+}
 
 fn console(){
     println!("Let's explore the world of solana\n");
     println!("Would you like to send (s) or recieve (r) BTC? : ");
     
     let mut command = String::new();
+
     io::stdin().read_line(&mut command);
 
+    if command.trim().eq("s"){
+        send_bitcoin();
+    }else if command.trim().eq("r"){
+recieve_bitcoin();
+
+    }
+    else{
+        exit_console();
+    }
 }
 
 fn main() {
     println!("Hello, world!");
-
+console();
 }
